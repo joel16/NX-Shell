e686cad65def7d1b070bbc95cafb4dfa6b78fe9d
Joel16 <sauskethunderkid@gmail.com>
2018-06-03 21:01:03 -0500

    Fix issue with selector not reaching the very last file

--- source/menus/menu_main.c
+++ source/menus/menu_main.c
@@ -21,16 +21,16 @@ static void Menu_ControlMenuBar(u64 input)
 
 static void Menu_DisplayMenuBar(void)
 {
-	SDL_DrawRect(RENDERER, 0, 0, 400, 720, dark_theme? BLACK_BG : WHITE);
-	SDL_DrawRect(RENDERER, 400, 0, 1, 720, dark_theme? TEXT_MIN_COLOUR_DARK : TEXT_MIN_COLOUR_LIGHT);
+	SDL_DrawRect(RENDERER, 0, 0, 400, 720, config_dark_theme? BLACK_BG : WHITE);
+	SDL_DrawRect(RENDERER, 400, 0, 1, 720, config_dark_theme? TEXT_MIN_COLOUR_DARK : TEXT_MIN_COLOUR_LIGHT);
 	SDL_DrawImage(RENDERER, bg_header, 0, 0, 400, 214);
 	SDL_DrawText(Roboto_large, 15, 164, WHITE, "NX Shell");
-	SDL_DrawImage(RENDERER, dark_theme? icon_sd_dark : icon_sd, 20, 254, 60, 60);
-	SDL_DrawText(Roboto, 100, 254, dark_theme? WHITE : BLACK, "External storage");
-	SDL_DrawText(Roboto_small, 100, 284, dark_theme? TEXT_MIN_COLOUR_DARK : TEXT_MIN_COLOUR_LIGHT, "sdmc");
+	SDL_DrawImage(RENDERER, config_dark_theme? icon_sd_dark : icon_sd, 20, 254, 60, 60);
+	SDL_DrawText(Roboto, 100, 254, config_dark_theme? WHITE : BLACK, "External storage");
+	SDL_DrawText(Roboto_small, 100, 284, config_dark_theme? TEXT_MIN_COLOUR_DARK : TEXT_MIN_COLOUR_LIGHT, "sdmc");
 	int settings_width = 0, settings_height = 0;
-	SDL_DrawRect(RENDERER, 10, 630, 80, 80, dark_theme? SELECTOR_COLOUR_DARK : SELECTOR_COLOUR_LIGHT);
-	SDL_DrawImage(RENDERER, dark_theme? icon_settings_dark : icon_settings, 20, 640, 60, 60);
+	SDL_DrawRect(RENDERER, 10, 630, 80, 80, config_dark_theme? SELECTOR_COLOUR_DARK : SELECTOR_COLOUR_LIGHT);
+	SDL_DrawImage(RENDERER, config_dark_theme? icon_settings_dark : icon_settings, 20, 640, 60, 60);
 }
 
 static void Menu_HomeControls(u64 input)
@@ -48,12 +48,12 @@ static void Menu_HomeControls(u64 input)
 
 			// Rewind Pointer
 			else 
-				position = fileCount - 1;
+				position = ((strcmp(cwd, ROOT_PATH) == 0? (fileCount - 1) : fileCount));
 		}
 		else if (input & KEY_DDOWN)
 		{
 			// Increase Position
-			if (position < (fileCount - 1))
+			if (position < ((strcmp(cwd, ROOT_PATH) == 0? (fileCount - 1) : fileCount)))
 				position++;
 
 			// Rewind Pointer
@@ -112,10 +112,10 @@ void Menu_Main(void)
 
 	while(appletMainLoop())
 	{
-		SDL_ClearScreen(RENDERER, dark_theme? BLACK_BG : WHITE);
+		SDL_ClearScreen(RENDERER, config_dark_theme? BLACK_BG : WHITE);
 		SDL_RenderClear(RENDERER);
-		SDL_DrawRect(RENDERER, 0, 0, 1280, 40, dark_theme? STATUS_BAR_DARK : STATUS_BAR_LIGHT);	// Status bar
-		SDL_DrawRect(RENDERER, 0, 40, 1280, 100, dark_theme? MENU_BAR_DARK : MENU_BAR_LIGHT);	// Menu bar
+		SDL_DrawRect(RENDERER, 0, 0, 1280, 40, config_dark_theme? STATUS_BAR_DARK : STATUS_BAR_LIGHT);	// Status bar
+		SDL_DrawRect(RENDERER, 0, 40, 1280, 100, config_dark_theme? MENU_BAR_DARK : MENU_BAR_LIGHT);	// Menu bar
 		
 		StatusBar_DisplayTime();
 		Dirbrowse_DisplayFiles();
