987ec517d5f804fc146c0a351cd141181c82c991
Joel16 <sauskethunderkid@gmail.com>
2018-11-10 01:36:00 -0600

    Fix mupdf path for travis

--- .travis/.build.sh
+++ .travis/.build.sh
@@ -5,7 +5,7 @@ sudo apt-get -y install build-essential
 source /etc/profile.d/devkit-env.sh
 export DEVKITA64=/opt/devkitpro/devkitA64
 
-cd NX-Shell/mupdf/
+cd mupdf/
 git checkout 1.14.0 && make generate
 cd ../ && make
 
