3e4250639b05374ca193fbc4acf17c8e58cafe34
Joel16 <sauskethunderkid@gmail.com>
2018-12-18 18:09:24 -0600

    Travis: Revert back to using docker

--- .travis/.build.sh
+++ .travis/.build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -ex
 
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get -y install build-essential
 source /etc/profile.d/devkit-env.sh
 export DEVKITA64=/opt/devkitpro/devkitA64
 
-cd mupdf/
-git checkout 1.14.0 && make generate
+cd /NX-Shell
+cd mupdf/ && git checkout 1.14.0 && make generate
 cd ../ && make
 
 rm -rf .travis build include mupdf romfs source
-rm .gitattributes .gitignore .gitmodules devkitpro-pacman.deb icon.jpg LICENSE Makefile Makefile.mupdf NX-Shell.elf NX-Shell.nacp NX-Shell.nso NX-Shell.pfs0 README.md .travis.yml
+rm .gitattributes .gitignore .gitmodules icon.jpg LICENSE Makefile Makefile.mupdf NX-Shell.elf NX-Shell.nacp NX-Shell.nso NX-Shell.pfs0 README.md .travis.yml
