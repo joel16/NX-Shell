0721086fb76db02b402da8c7034514a4f308f5f1
Joel16 <joel16@iastate.edu>
2020-08-29 23:59:40 -0400

    windows: Disable window padding and titlebar when displaying images

--- source/windows/image.cpp
+++ source/windows/image.cpp
@@ -6,9 +6,11 @@ namespace Windows {
     void ImageWindow(Tex *texture) {
         Windows::SetupWindow();
         
-        if (ImGui::Begin(item.selected_filename.c_str(), nullptr, ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse))
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0, 0));
+        if (ImGui::Begin(item.selected_filename.c_str(), nullptr, ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoTitleBar))
             ImGui::Image(reinterpret_cast<ImTextureID>(texture->id), ImVec2(texture->width, texture->height));
             
         Windows::CloseWindow();
+        ImGui::PopStyleVar();
 	}
 }
