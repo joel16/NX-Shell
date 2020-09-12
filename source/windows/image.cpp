7ffb10b7245551bd448ddae6422e0f36fb883ea0
Joel16 <joel16@iastate.edu>
2020-09-12 17:10:10 -0400

    textures: Center image in image viewer

--- source/windows/image.cpp
+++ source/windows/image.cpp
@@ -3,14 +3,20 @@
 #include "imgui.h"
 #include "windows.h"
 
+#define IMGUI_DEFINE_MATH_OPERATORS
+#include "imgui_internal.h"
+
 namespace Windows {
     void ImageWindow(Tex *texture) {
         Windows::SetupWindow();
         
         ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0, 0));
         ImGuiWindowFlags_ filename_flag = !config.image_filename? ImGuiWindowFlags_NoTitleBar : ImGuiWindowFlags_None; 
-        if (ImGui::Begin(item.selected_filename.c_str(), nullptr, ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse | filename_flag))
+        
+        if (ImGui::Begin(item.selected_filename.c_str(), nullptr, ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse | filename_flag)) {
+            ImGui::SetCursorPos((ImGui::GetWindowSize() - ImVec2(texture->width, texture->height)) * 0.5f);
             ImGui::Image(reinterpret_cast<ImTextureID>(texture->id), ImVec2(texture->width, texture->height));
+        }
         
         Windows::ExitWindow();
         ImGui::PopStyleVar();
