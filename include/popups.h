3c0cdaac9c30443adda3a665598a7a1b02984643
Joel16 <joel16@iastate.edu>
2020-09-13 21:50:59 -0400

    popups: Fix popups not always being centered

--- include/popups.h
+++ include/popups.h
@@ -6,14 +6,14 @@
 namespace Popups {
     inline void SetupPopup(const char *id) {
         ImGui::OpenPopup(id);
-		ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(15, 15));
-		ImGui::SetNextWindowPos(ImVec2(640.0f, 360.0f), ImGuiCond_Once, ImVec2(0.5f, 0.5f));
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(15, 15));
+        ImGui::SetNextWindowPos(ImVec2(640.0f, 360.0f), ImGuiCond_Appearing, ImVec2(0.5f, 0.5f));
     };
-
+    
     inline void ExitPopup(void) {
         ImGui::EndPopup();
         ImGui::PopStyleVar();
-    }
+    };
 
     void ArchivePopup(void);
     void DeletePopup(void);
--- source/windows/image.cpp
+++ source/windows/image.cpp
@@ -11,7 +11,7 @@ namespace Windows {
         Windows::SetupWindow();
         
         ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0, 0));
-        ImGuiWindowFlags_ filename_flag = !config.image_filename? ImGuiWindowFlags_NoTitleBar : ImGuiWindowFlags_None; 
+        ImGuiWindowFlags_ filename_flag = !config.image_filename? ImGuiWindowFlags_NoTitleBar : ImGuiWindowFlags_None;
         
         if (ImGui::Begin(item.selected_filename.c_str(), nullptr, ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse | filename_flag)) {
             ImGui::SetCursorPos((ImGui::GetWindowSize() - ImVec2(texture->width, texture->height)) * 0.5f);
