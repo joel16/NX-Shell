27898a179bfd352dcfcdb1062fa4fd22c8e12601
Joel16 <joel16dev@gmail.com>
2021-09-22 22:10:02 -0400

    popups: Use ImGuiCond_Always when resizing popups

--- include/popups.h
+++ include/popups.h
@@ -10,7 +10,7 @@ namespace Popups {
     inline void SetupPopup(const char *id) {
         ImGui::OpenPopup(id);
         ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(15, 15));
-        ImGui::SetNextWindowPos(ImVec2(640.0f, 360.0f), ImGuiCond_Appearing, ImVec2(0.5f, 0.5f));
+        ImGui::SetNextWindowPos(ImVec2(640.0f, 360.0f), ImGuiCond_Always, ImVec2(0.5f, 0.5f));
     };
     
     inline void ExitPopup(void) {
