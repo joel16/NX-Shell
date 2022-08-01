663ba14b19caa7090eed050a680960cdf45f0511
Joel16 <joel16dev@gmail.com>
2022-08-01 12:02:48 -0400

    progressbar: Fix indentation

--- source/popups/progressbar.cpp
+++ source/popups/progressbar.cpp
@@ -9,11 +9,11 @@
 
 // Todo maybe use a thread to run this?
 namespace Popups {
-
+    
     void ProgressBar(float offset, float size, const std::string &title, const std::string &text) {
         u64 key = ImGui_ImplSwitch_NewFrame();
         ImGui::NewFrame();
-
+        
         Windows::MainWindow(data, key, true);
         Popups::SetupPopup(title.c_str());
         
@@ -21,13 +21,13 @@ namespace Popups {
             ImGui::Text(text.c_str());
             ImGui::ProgressBar(offset/size, ImVec2(0.0f, 0.0f));
         }
-		
-		Popups::ExitPopup();
-
+        
+        Popups::ExitPopup();
+        
         ImGui::Render();
-		glViewport(0, 0, static_cast<int>(ImGui::GetIO().DisplaySize.x), static_cast<int>(ImGui::GetIO().DisplaySize.y));
-		glClearColor(0.00f, 0.00f, 0.00f, 1.00f);
-		glClear(GL_COLOR_BUFFER_BIT);
+        glViewport(0, 0, static_cast<int>(ImGui::GetIO().DisplaySize.x), static_cast<int>(ImGui::GetIO().DisplaySize.y));
+        glClearColor(0.00f, 0.00f, 0.00f, 1.00f);
+        glClear(GL_COLOR_BUFFER_BIT);
         ImGui_ImplSwitch_RenderDrawData(ImGui::GetDrawData());
         GUI::SwapBuffers();
     }
